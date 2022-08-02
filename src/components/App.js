58764f58b598fbc34b2852a7a7d8a6646fe6cd2b
Daivison Morais <daivison13pinheiro@gmail.com>
2022-08-02 17:47:17 -0300

    preparativos prontos

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../assets/css/styles.css";
+import "../assets/css/reset.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App () {
+return (
+    <>
+    
+    </>
+)
+}
